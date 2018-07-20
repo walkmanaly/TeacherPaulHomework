e5098788d5f508d038d84e8c4d35213887ed8a52
Nick <walkmanaly@gmail.com>
2018-07-20 17:51:36 +0800

    change color style
    change color style

--- TeacherPaulHomework/TeacherPaulHomework/ViewController.swift
+++ TeacherPaulHomework/TeacherPaulHomework/ViewController.swift
@@ -19,10 +19,10 @@ class ViewController: UIViewController {
     func concentrat(withTitle title: String, sender btn: UIButton) {
         if title == "👻" {
             btn.setTitle("", for: UIControlState.normal)
-            btn.backgroundColor = UIColor.white
+            btn.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         } else {
             btn.setTitle("👻", for: UIControlState.normal)
-            btn.backgroundColor = UIColor.orange;
+            btn.backgroundColor = #colorLiteral(red: 0.9450980392, green: 0.3490196078, blue: 0.1568627451, alpha: 1)
         }
     }
 
